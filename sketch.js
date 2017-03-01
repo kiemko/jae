46805f451e8deb362afbfcedd56c89392a6e3971
kiemko <jeakiemkoko@gmail.com>
2017-03-01 23:22:50 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 function Ball(x, y) {
   this.position = {x: x, y: y};
   this.speed = {x:random(-2,2),y:(-2,2)},
+  
 
   this.draw = function() {
+         if(this.position.y > height){
+       this.speed.y *= -1;
+     };
+     if(this.position.x > width){
+       this.speed.x *= -1;
+    };
+     if(this.position.x < 0){
+     this.speed.x += 1
+     };
+     if(this.position.y < 0){
+    this.speed.y += 1
+     };
+     
+   this.position.x += this.speed.x;
+   this.position.y += this.speed.y;
  this.position.x = this.position.x + this.speed.x + random (-2,2);
  this.position.y = this.position.y + this.speed.y +random(-2,2);
     ellipse(this.position.x, this.position.y, random(-25,25),25);
@@ -10,16 +26,19 @@ function Ball(x, y) {
 }
 
 var balls = [];
-function mousePressed() {
-  stroke(100,100,100,100);
-  noFill();
+function mouseDragged() {
+  
   var ball = new Ball(mouseX, mouseY);
   balls.push(ball);
 }
 
 function draw() {
+  background(255);
+  stroke(122, 79, 124);
+  fill(255,212,216)
   for (var i = 0; i < balls.length; i++)
     balls[i].draw();
+    
 }
 
 
