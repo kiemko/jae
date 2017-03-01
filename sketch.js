4d1c755bc412913234d9b16b5e0bd75c6e683c16
kiemko <jeakiemkoko@gmail.com>
2017-03-01 23:31:41 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,21 @@
-function Ball(x, y) {
-  this.position = {x: x, y: y};
-  this.speed = {x:random(-2,2),y:(-2,2)},
-
-  this.draw = function() {
- this.position.x = this.position.x + this.speed.x + random (-2,2);
- this.position.y = this.position.y + this.speed.y +random(-2,2);
-    ellipse(this.position.x, this.position.y, random(-25,25),25);
-  }
-}
-
-var balls = [];
-function mousePressed() {
-  stroke(100,100,100,100);
-  noFill();
-  var ball = new Ball(mouseX, mouseY);
-  balls.push(ball);
-}
-
-function draw() {
-  for (var i = 0; i < balls.length; i++)
-    balls[i].draw();
-}
-
-
 function setup() {
-  resizeCanvas(700, 700);
+  resizeCanvas(1000,1000);
+  setInterval(loadISS, 1000);
+  noStroke();
+}
+
+function loadISS(){
+  loadJSON('http://api.open-notify.org/iss-now.json',gotISS);
+}
+
+function gotISS(data) {
+  var longitude = Number(data.iss_position.longitude);
+  var latitude = Number(data.iss_position.latitude);
+  
+  //ellipse(50,50,latitude,latitude);
+  fill (random(255), random(255), random(255), random(255));
+  var x = map(longitude, -180, 180, 0, 500);
+  var y = map(latitude, 90, -90, 0, 500);
+  rect(x,y,random(250),random(250));
+  
 }
